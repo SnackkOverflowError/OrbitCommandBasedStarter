7271186929a5c288879ec910b38f8c56418c95df
johnnchung <31940951+johnnchung@users.noreply.github.com>
2019-11-14 20:48:24 -0500

    Subsystems
    Updated code

--- src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -1,13 +1,7 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.Victor;
 
 /**
  * Add your docs here.
@@ -15,10 +9,23 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class DrivetrainSubsystem extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
+  private Victor motorLeft = new Victor(1);
+  private Victor motorRight = new Victor(2);
+
+
+  //Drive function
+  public void drive(double speed, double turn){
+
+    // Setting motors
+    motorLeft.set(speed + turn);
+    motorRight.set(speed + turn);
+  }
 
   @Override
   public void initDefaultCommand() {
     // Set the default command for a subsystem here.
     // setDefaultCommand(new MySpecialCommand());
+
+    setDefaultCommand(new DriveCommand());
   }
 }
